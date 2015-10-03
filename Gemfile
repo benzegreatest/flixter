1e61b8861a0b7f23dd84b643b47c6d5a3c889af3
vagrant <vagrant@precise32.(none)>
2015-10-03 18:19:00 +0000

    updating for video

--- Gemfile
+++ Gemfile
@@ -10,8 +10,6 @@ gem "figaro"
 
 gem 'mini_magick'
 
-gem "fog", "~> 1.3.1"
-
 gem 'simple_form'
 
 gem 'devise', '>= 3.2.4'
@@ -20,6 +18,8 @@ gem 'devise', '>= 3.2.4'
 gem 'sass-rails', '~> 4.0.0'
 
 gem 'carrierwave'
+
+gem 'carrierwave-aws'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AKIAJRIBX3JZLAZ3Q3DQ"],                        # required
-    :aws_secret_access_key  => ENV["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"]                         # required
+  config.storage    = :aws
+  config.aws_bucket = ENV["flixterbucket"]
+  config.aws_acl    = "public-read"
+
+  config.aws_credentials = {
+      access_key_id:     ENV["AKIAJRIBX3JZLAZ3Q3DQ"],
+      secret_access_key: ENV["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"],
+      region:            ENV["us-east-1"]
   }
-  config.fog_directory  = ENV["flixterbucket"]                     # required
 end
